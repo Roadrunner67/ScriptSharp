54f0313297c43d86bb72e000fe3242c33f5a1d06
Søren Bech Christensen <sbc@bangding.dk>
2018-12-04 22:33:06 +0100

    ConsoleApp added for exploring. ReadVal Func injected via Globals.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using ScriptSharp.ScriptEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpScriptEngine.Execute(
+            //This could be code submitted from the editor
+            //    @"using System;
+
+            //public class ScriptedClass
+            //{
+            //    public string HelloWorld {get;set;}
+            //    public ScriptedClass()
+            //    {
+            //        Console.WriteLine($""Initializing the {nameof(ScriptedClass)} class"");
+            //        HelloWorld = ""Hello Roslyn!"";
+            //    }
+
+            //    public string GreetSoren()
+            //    {
+            //        return ""Hello Søren"";
+            //    }
+            //}"
+            @"using System;
+
+            public class CommTest
+            {
+                Func<string, Double> _readValFunc;
+
+                public CommTest(Func<string, Double> readValFunc)
+                {
+                    _readValFunc = readValFunc;
+                }
+                public Double GetAValue(string identifier)
+                {
+                    return _readValFunc(identifier);
+                }
+            }
+            //Console.WriteLine(new CommTest(ReadVal).GetAValue(""Test""));
+            "
+            );
+            //And this from the REPL
+            //Console.WriteLine(CSharpScriptEngine.Execute("new ScriptedClass().HelloWorld"));
+            //Console.WriteLine(CSharpScriptEngine.Execute("new ScriptedClass().GreetSoren()"));
+            Console.WriteLine(CSharpScriptEngine.Execute("new CommTest(ReadVal).GetAValue(\"Test\").ToString()"));
+
+            Console.ReadKey();
+        }
+    }
+    //Output: "Hello Roslyn!"
+
+}
--- src/ScriptEngine/CSharpScriptEngine.cs
+++ src/ScriptEngine/CSharpScriptEngine.cs
@@ -4,12 +4,31 @@ using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace ScriptSharp.ScriptEngine
 {
+    public class Global
+    {
+        public int X;
+        public Func<string, Double> ReadVal;
+    }
     public class CSharpScriptEngine
     {
+        public static Double MyReader(string Id)
+        {
+            switch (Id)
+            {
+                case "Test":
+                    return 100.0;
+                case "Test2":
+                    return 37.5;
+                default:
+                    return 1.0;
+            }
+        }
+
         private static ScriptState<object> scriptState = null;
         public static object Execute(string code)
         {
-            scriptState = scriptState == null ? CSharpScript.RunAsync(code).Result : scriptState.ContinueWithAsync(code).Result;
+            Global global = new Global { X = 3, ReadVal = MyReader };
+            scriptState = scriptState == null ? CSharpScript.RunAsync(code: code, globals: global).Result : scriptState.ContinueWithAsync(code: code).Result;
             if (scriptState.ReturnValue != null && !string.IsNullOrEmpty(scriptState.ReturnValue.ToString()))
                 return scriptState.ReturnValue;
             return null;
